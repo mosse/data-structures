dc02d7cec3ec305c8ee6d1d27e3a70544997c54e
mosse <mjemacdonald@gmail.com>
2015-02-04 14:59:25 -0800

    Implementing enqueue and size for queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,20 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var head = 0;
+  var tail = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[tail++] = value;
   };
 
   someInstance.dequeue = function(){
   };
 
   someInstance.size = function(){
+    return tail - head;
   };
 
   return someInstance;
