a95a7005e65e2c7f7ef72add42fe8a533107a957
mosse <mjemacdonald@gmail.com>
2015-02-04 16:29:20 -0800

    Add queue unique properties for functional-shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,6 +1,10 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var head = 0;
+  var tail = 0;
+  var obj = {head: head, tail: tail};
+  var _.extend(obj, queueMethods);
 };
 
 var queueMethods = {};
