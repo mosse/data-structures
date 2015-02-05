b00e72f8ae3ecf8fc0626df174fff01ea34f989d
mosse <mjemacdonald@gmail.com>
2015-02-04 16:18:11 -0800

    Finishing push and pop

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -2,9 +2,23 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var storage = {};
-  var size = 0;
+  var numElements = 0;
+  var obj = {storage: storage, numElements: numElements};
+  return _.extend(obj, stackMethods);
+
 };
 
-var stackMethods = {};
+var stackMethods = {
+  size: function() {
+    return this.numElements;
+  },
+  push: function(val) {
+    this.storage[this.numElements++] = val;
+  },
+  pop: function() {
+    this.numElements && this.numElements--;
+    return this.storage[this.numElements];
+  }
+};
 
 
