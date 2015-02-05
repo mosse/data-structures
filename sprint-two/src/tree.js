edc78b2a44db6b0fd990fa86527b8358bb0402e6
mosse <mjemacdonald@gmail.com>
2015-02-05 15:38:34 -0800

    Tree - Extending newTree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,10 +2,11 @@ var Tree = function(value){
   var newTree = {};
   newTree.value = value;
 
+
   // your code here
   newTree.children = null;  // fix me
 
-  return newTree;
+  return _.extend(newTree, treeMethods);
 };
 
 
