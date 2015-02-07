ed925e4a2ec7403ae862ca9963cb4db91c08d721
mosse <mjemacdonald@gmail.com>
2015-02-06 19:10:37 -0800

    BST - Implement insert and create skeleton methods

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,10 +1,35 @@
 var BinarySearchTree = function(value){
-  this._val = value;
-  this._left = null;
-  this._right = null;
+  this.value = value;
+  this.left = null;
+  this.right = null;
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+BinarySearchTree.prototype.insert = function (value) {
+  var node = new BinarySearchTree(value);
+  if (value <= this.value){
+    if (this.left === null){
+      this.left = node;
+    } else {
+      this.left.insert(value);
+    }
+  } else {
+    if (this.right === null){
+      this.right = node;
+    } else {
+      this.right.insert(value);
+    }
+  }
+};
+
+BinarySearchTree.prototype.contains = function(value) {
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(callback) {
+
+};
