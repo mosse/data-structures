b02a0e2a07b6a74e84032be4076fe25f675d2d18
mosse <mjemacdonald@gmail.com>
2015-02-06 16:32:17 -0800

    HashTable - Implement add and retrieve

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,11 +5,20 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  // at index i of storage, set the tuple as (k, v)
+  this._storage[i] = this._storage[i] || [];
+  this._storage[i].push([k, v]);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var result;
+  _.each(this._storage[i], function(tuple){
+    if (tuple[0] === k){
+      result = tuple[1];
+    }
+  });
+  return result;
 };
 
 HashTable.prototype.remove = function(k){
